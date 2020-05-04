328129926057c63b650b4d5cda7adc75768b9fbe
Taigah <taigah.dev@gmail.com>
2020-05-04 11:13:43 +0200

    fix: fix encoding issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { randomBytes } = require('crypto')
 const { promisify } = require('util')
 const path = require('path')
 const os = require('os')
+const { createWriteStream } = require('fs')
 const fs = require('fs').promises
 const { once } = require('events')
 
@@ -14,21 +15,19 @@ const tmpfile = path.join(os.tmpdir(), randomBytes(10).toString('hex'))
 async function run () {
   // gets the hex dump of the file
   // then writes it in a temp file
-  const { stdout: hexdumpStdout } = await promisify(exec)(`xxd "${srcfile}"`)
-  await fs.writeFile(tmpfile, hexdumpStdout)
+  await promisify(exec)(`xxd "${srcfile}" > "${tmpfile}"`)
   
   // opens the temp file using the user's default editor
   const proc = spawn($editor, [tmpfile], {
     stdio: 'inherit'
   })
-  
+
   // waits for the user to close his editor
   await once(proc, 'close')
 
   // when he closes the editor, converts back the hex dump to binary
   // then writes it back in the srcfile
-  const { stdout: binStdout } = await promisify(exec)(`xxd -r "${tmpfile}"`)
-  await fs.writeFile(srcfile, binStdout)
+  await promisify(exec)(`xxd -r "${tmpfile}" > "${srcfile}"`)
 }
 
 run().catch(err => {
